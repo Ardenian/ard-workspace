0cd11e5c49237dd83dfcc0f3d5ec8a94e2353cdb
Ardenian <22526933+Ardenian@users.noreply.github.com>
2024-09-05 14:43:50 +0200

    remove obsolete tests

--- projects/ard-prototype/src/app/app.component.ts
+++ projects/ard-prototype/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TestComponent } from '../features/test/test.component';
 
 @Component({
   selector: 'ard-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
